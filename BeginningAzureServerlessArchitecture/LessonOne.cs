b6bbca0eba901bdfdd5ddc736b793663a3b4d556
Daniel Bass <danbass8@googlemail.com>
2018-01-26 12:42:09 +0000

    Debugging an Azure Function

--- BeginningAzureServerlessArchitecture/LessonOne.cs
+++ BeginningAzureServerlessArchitecture/LessonOne.cs
@@ -1,21 +1,26 @@
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using BeginningAzureServerlessArchitecture.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
 
 namespace BeginningAzureServerlessArchitecture
 {
     public static class LessonOne
     {
         [FunctionName("LessonOne")]
-        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "HttpTriggerCSharp/name/{name}")]HttpRequestMessage req, string name, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Transactions")]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            // Fetching the name from the path parameter in the request URL
-            return req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+            var message = await req.Content.ReadAsStringAsync();
+            var transaction = JsonConvert.DeserializeObject<Transaction>(message);
+
+            return req.CreateResponse(HttpStatusCode.OK, $"You made a transaction of £{transaction.Amount}!");
         }
     }
 }
